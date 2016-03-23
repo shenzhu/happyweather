fea5dfd235f466683a4cf71178a0dad910bb4ac9
Shen Zhu <shenzhu0127@gmail.com>
2016-03-23 15:51:12 +0800

    show weather

--- app/src/main/java/com/shenzhu/android/happyweather/activity/util/Utility.java
+++ app/src/main/java/com/shenzhu/android/happyweather/activity/util/Utility.java
@@ -1,11 +1,21 @@
 package com.shenzhu.android.happyweather.activity.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.shenzhu.android.happyweather.activity.db.HappyWeatherDB;
 import com.shenzhu.android.happyweather.activity.model.City;
 import com.shenzhu.android.happyweather.activity.model.County;
 import com.shenzhu.android.happyweather.activity.model.Province;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Utility {
     /**
@@ -71,5 +81,41 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * Parse JSON data from server
+     */
+    public static void handleWeatherResponse(Context context, String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+            String cityName = weatherInfo.getString("city");
+            String weatherCode = weatherInfo.getString("cityid");
+            String temp1 = weatherInfo.getString("temp1");
+            String temp2 = weatherInfo.getString("temp2");
+            String weatherDesp = weatherInfo.getString("weather");
+            String publishTime = weatherInfo.getString("ptime");
+            saveWeatherInfo(context, cityName, weatherCode, temp1, temp2, weatherDesp, publishTime);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Save weather data to SharedPreferences
+     */
+    public static void saveWeatherInfo(Context context, String cityName, String weatherCode, String temp1,
+                                       String temp2, String weatherDesp, String publishTime){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd", Locale.CHINA);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putBoolean("city_selected", true);
+        editor.putString("city_name", cityName);
+        editor.putString("weather_code", weatherCode);
+        editor.putString("temp1", temp1);
+        editor.putString("temp2", temp2);
+        editor.putString("weather_desp", weatherDesp);
+        editor.putString("publish_time", publishTime);
+        editor.putString("current_date", sdf.format(new Date()));
     }
 }
